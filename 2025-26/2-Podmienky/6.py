b5d878563c60a2c007348429781f14fe882de126
JanMacicka <jan.macicka@gmail.com>
2025-09-16 22:11:55 +0200

    Revert "Update 6.py"
    This reverts commit 2be50c6ccc7f784af97c4a0a12477ba84a9ab296.

--- 2025-26/2-Podmienky/6.py
+++ 2025-26/2-Podmienky/6.py
@@ -6,6 +6,8 @@ day = int(input("zadajte den: "))
 for i in range(len(days) - 1):
     sum += days[i]
 
-    if day > sum:
-        print(f"{day - sum}.{i + 2}.")
+    if day > sum and day <= sum + days[i + 1]:
+        days_in_month = day - sum
+
+        print(f"{days_in_month}.{i + 2}.")
         break
