d6eb7c5619a3e7d9dff8f53508cbab85eddac9be
JanMacicka <78503911+JanMacicka@users.noreply.github.com>
2025-10-14 13:15:31 +0200

    8 - alt.

--- 2025-26/Ulohy/5-Polia/8.py
+++ 2025-26/Ulohy/5-Polia/8.py
@@ -2,32 +2,34 @@ import os
 import random
 
 money = int(input("servus, ty milovnik ovocicka, zadaj svoj vklad: "))
+numbers = [random.randint(1, 99) for _ in range(10)]
 correct = 0
-win = 1
+win_multiplier = 1
 
 for i in range(10):
     os.system("cls")
-
-    num = random.randint(0, 10)
+    
     guess = int(input("hadaj [1-10]: "))
 
-    if num == guess:
+    if guess in numbers:
         correct += 1
+
+        numbers.remove(guess)
 
 os.system("cls")
 
 match correct:
     case 10:
-        win = 200000
+        win_multiplier = 200000
     case 9:
-        win = 10000
+        win_multiplier = 10000
     case 8:
-        win = 500
+        win_multiplier = 500
     case 7:
-        win = 20
+        win_multiplier = 20
     case 6:
-        win = 10
+        win_multiplier = 10
     case 5:
-        win = 3
+        win_multiplier = 3
 
-print(f"vyhral si {money * win} €. uhadol si {correct} cisel.")
+print(f"vyhral si {money * win_multiplier} €. uhadol si {correct} cisel.")
