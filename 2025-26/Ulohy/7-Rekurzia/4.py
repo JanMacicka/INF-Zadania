d9382b364e27853f9a6c7771423d68fc8a285e7e
JanMacicka <78503911+JanMacicka@users.noreply.github.com>
2025-11-18 13:09:48 +0100

    7.4

--- 2025-26/Ulohy/7-Rekurzia/4.py
+++ 2025-26/Ulohy/7-Rekurzia/4.py
@@ -1,5 +1,5 @@
-def array_sum(array: list[int] = [0]) -> int:
-    if len(array) == 1:
+def array_sum(array: list[int] = []) -> int:
+    if len(array) <= 1:
         return array[0]
     else:
         return array[0] + array_sum(array[1:])
