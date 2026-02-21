1e5a641adb6c78c9652378f62a5271a4b9c48e29
JanMacicka <jan.macicka@gmail.com>
2026-02-21 22:09:36 +0100

    9.21

---
+++ 2025-26/Ulohy/9-Grafika/21.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+
+
+WIDTH = 700
+HEIGHT = 500
+
+FLAG_WIDTH = 325
+FLAG_HEIGHT = 216
+OFFSET = 30
+
+root = tk.Tk()
+canvas = tk.Canvas(root, width=WIDTH, height=HEIGHT)
+
+canvas.pack()
+
+canvas.create_rectangle(OFFSET, OFFSET, OFFSET + FLAG_WIDTH, OFFSET + FLAG_HEIGHT / 3, outline="")
+canvas.create_rectangle(OFFSET, OFFSET + FLAG_HEIGHT / 3, OFFSET + FLAG_WIDTH, OFFSET + 2 * FLAG_HEIGHT / 3, fill="#0b4ea2", outline="")
+canvas.create_rectangle(OFFSET, OFFSET + 2 * FLAG_HEIGHT / 3, OFFSET + FLAG_WIDTH, OFFSET + FLAG_HEIGHT, fill="#ee1c25", outline="")
+
+image = tk.PhotoImage(file=r"C:\Users\MK\Desktop\INF-Zadania\INF-Zadania\2025-26\Ulohy\9-Grafika\21-1.png")
+
+canvas.create_image(OFFSET + 100, OFFSET + 108, image=image)
+canvas.create_rectangle(OFFSET, OFFSET, OFFSET + FLAG_WIDTH, OFFSET + FLAG_HEIGHT)
+    
+root.mainloop()
