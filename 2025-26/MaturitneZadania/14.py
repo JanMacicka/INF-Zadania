f2d5b6f0be23d004edf206876a4a5f99127b64a6
JanMacicka <jan.macicka@gmail.com>
2026-03-24 11:41:41 +0100

    14

--- 2025-26/MaturitneZadania/14.py
+++ 2025-26/MaturitneZadania/14.py
@@ -3,6 +3,9 @@ import tkinter as tk
 
 WINDOW_WIDTH = 480
 WINDOW_HEIGHT = 520
+GRAPH_HEIGHT = WINDOW_HEIGHT - 20
+COLUMN_WIDTH = WINDOW_WIDTH / 24
+HEIGHT_MULTIPLIER = 50
 
 root = tk.Tk()
 canvas = tk.Canvas(root, width=WINDOW_WIDTH, height=WINDOW_HEIGHT)
@@ -26,7 +29,7 @@ worst_hour = 0
 print(f"pocet negativnych vyjadreni: {sum(reviews_by_hour.values())}")
 print("hodiny s negativnymi vyjadreniami:")
 
-for hour in reviews_by_hour:
+for i,  hour in enumerate(reviews_by_hour):
     review_count = reviews_by_hour[hour]
 
     if review_count > reviews_by_hour[worst_hour]:
@@ -35,7 +38,13 @@ for hour in reviews_by_hour:
     if review_count > 0:
         print(f"{hour}. - {reviews_by_hour[hour]} negativnych hodnoteni")
 
-    # tkinter
+    x1 = COLUMN_WIDTH * i
+    x2 = x1 + COLUMN_WIDTH
+    y1 = GRAPH_HEIGHT
+    y2 = y1 - review_count * HEIGHT_MULTIPLIER
+
+    canvas.create_rectangle(x1, y1, x2, y2, fill="red", outline="black")
+    canvas.create_text(x1 + COLUMN_WIDTH / 2, y1, anchor="n", text=f"{i:02d}")
 
 print(f"najhorsia hodina: {worst_hour}. - {reviews_by_hour[worst_hour]} negativnych hodnoteni")
 
